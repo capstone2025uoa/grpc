be45a5abb668538ab68a38f3beba39b1c04bea7b
naruto716 <79365555+naruto716@users.noreply.github.com>
2025-02-28 12:39:23 +1300

    Add C# gRPC code generation and dynamic module import

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='compsci399_grpc',
-    version='0.1.0',
+    version='0.2.0',
     packages=['compsci399_grpc'],
     package_dir={'compsci399_grpc': 'generated/compsci399_grpc'},
     install_requires=[
